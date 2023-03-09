404df710884377a5c29f5d0b584b1a00c4e252e2
javad <javadMohammadi.js@gmail.com>
2023-03-09 16:45:34 +0330

    fix bugs when remove & concat others query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,97 +9,73 @@ const styles = {
 }
 function Header({ params, setParams }) {
   const inpref = useRef()
-  const newParams = { id: uuidv4(), key: '', value: '', description: '' }
-
+  let paramterStr = ''
   const setInputRequsetHandler = (event) => {
+    const newParams = { id: uuidv4(), key: '', value: '', description: '' }
     const currentValue = inpref.current.value
     const findquery = /[?]+/.exec(currentValue)
+
     if (findquery !== null) {
       const indexofquery = findquery.index
       const currentQuery = findquery.input
-      const allParams = currentQuery.slice(indexofquery + 1)
-      if (params.length === 0) {
+      paramterStr = currentQuery.slice(indexofquery + 1)
+
+      if (params.length === 1) {
         setParams([...params, newParams])
       }
-      splitParamsToTable(allParams)
+
+      splitParamsToTable(paramterStr)
     } else {
-      if (params.length === 1) {
+      if (params.length > 1) {
         const allParams = params.splice(-1, 1)
-        console.log(allParams)
-        setParams([])
+        setParams(allParams)
       }
     }
   }
-
   const splitParamsToTable = (paramsText) => {
     const allParams = paramsText
     const findquery = /[&]+/.exec(allParams)
     if (findquery !== null) {
       const paramsList = allParams.split('&')
-      paramsList.map((param) => {
-        const isAddEqual = /[=]+/.exec(param)
-        let key = param
-        if (isAddEqual) {
-          key = param.slice(0, isAddEqual.index)
-          const value = param.slice(isAddEqual.index + 1)
-          let allParams = params
-          let newParam = { id: uuidv4, key, value, description: '' }
-          setParams([...allParams, newParam])
+      let item = { id: uuidv4(), key: '', value: '', description: '' }
+      const allparamsClone = params
+      paramsList.map((param, index) => {
+        let isequal = /[=]+/.exec(param)
+        if (isequal !== null) {
+          const key = param.slice(0, isequal.index)
+          const value = param.slice(isequal.index + 1)
+          item = { id: uuidv4(), key, value, description: '', index }
+          allparamsClone[index] = item
+        } else {
+          item = { id: uuidv4(), key: param, value: '', description: '', index }
+          allparamsClone[index] = item
         }
       })
+      setParams([...allparamsClone])
     } else {
       const isAddValue = /[=]+/.exec(allParams)
-      let query = ''
-      if (isAddValue) {
-        const indexofquery = isAddValue.index
-        const currentQuery = isAddValue.input
-        query = currentQuery.slice(indexofquery + 1)
-      }
-      const arrayOfParams = allParams.split('=')
-      let objparams = {
-        id: uuidv4(),
-        key: arrayOfParams[0],
-        value: query ? query : '',
-        decription: '',
-      }
-      const allparamsArray = []
-      allparamsArray.push(objparams)
+      if (isAddValue || allParams.length > 0) {
+        const arrayOfParams = allParams.split('=')
+        let objparams = {
+          id: uuidv4(),
+          key: arrayOfParams[0],
+          value: arrayOfParams[1],
+          decription: '',
+        }
+        const allparamsArray = []
+        allparamsArray.push(objparams, {
+          id: uuidv4(),
+          key: '',
+          value: '',
+          description: '',
+        })
 
-      setParams([...allparamsArray])
+        setParams([...allparamsArray])
+      }
     }
   }
-  const isremoveAndHandler = (evt) => {
-    let rez = []
-    const t = evt.target
-    if (evt.keyCode === 8) {
-      const deletedChar = t.value[t.selectionStart - 1]
-      if (deletedChar === '&') {
-        setTimeout(() => {
-          const findquery = /[?]+/.exec(inpref.current.value)
-          if (findquery) {
-            const splitValue = inpref.current.value
-              .slice(findquery.index + 1)
-              .split('&')
-            splitValue.map((list, index) => {
-              let isequal = /[=]+/.exec(list)
-              if (isequal !== null) {
-                const key = list.slice(0, isequal.index)
-                const value = list.slice(isequal.index + 1)
-                const item = { key, value, description: '', index }
-                rez.push(item)
-              }
-              setParams(rez)
-            })
-          }
-        }, 1)
-      }
-    } else if (evt.keyCode === 46) {
-      // for delete key
-      console.log(t.value[t.selectionStart])
-    }
-
-    // if add & to url add new key and value
-    if (evt.which === 55) {
+  const setqueryHandler = (evt) => {
+    if (evt.keyCode === 55) {
       const allparamsClone = params
       allparamsClone.push({
         id: uuidv4(),
@@ -109,14 +85,43 @@ function Header({ params, setParams }) {
       })
       setParams([...allparamsClone])
     }
+    let rez = []
+    const t = evt.target
+    if (evt.keyCode === 8) {
+      const deletedChar = t.value[t.selectionStart - 1]
+      if (deletedChar === '&') {
+        setTimeout(() => {
+          const splitParametrs = paramterStr.split('&')
+          console.log(splitParametrs)
+          splitParametrs.map((param, index) => {
+            let isEqual = /[=]+/.exec(param)
+            if (isEqual !== null) {
+              const key = param.slice(0, isEqual.index)
+              const value = param.slice(isEqual.index + 1)
+              const item = { id: uuidv4(), key, value, description: '' }
+              rez.push(item, {
+                id: uuidv4(),
+                key: '',
+                value: '',
+                description: '',
+              })
+              setParams([...rez])
+            }
+          })
+        }, 1)
+      }
+    } else if (evt.keyCode === 46) {
+      // for delete key
+    }
   }
   return (
     <div>
+      {/* input */}
       <input
         ref={inpref}
         onChange={(event) => setInputRequsetHandler(event)}
         style={styles}
-        onKeyDown={(event) => isremoveAndHandler(event)}
+        onKeyDown={(event) => setqueryHandler(event)}
       />
     </div>
   )
